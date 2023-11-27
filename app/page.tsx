0b9deb693b271c83de266a72fa8ec5d545ce5d27
FELIPE <contact@iapps.ai>
2023-11-27 02:39:48 -0500

    Ájuste de fuente

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <img
         src="/logo.png" // Asume que tu logo está en la carpeta public/images
         alt="Logo"
-        className="absolute left-0 top-0 m-4 w-12 h-12" // Margen de 4 y dimensiones de 12x12, posición izquierda superior
+        className="absolute left-0 top-0 m-5 w-12 h-12" // Margen de 4 y dimensiones de 12x12, posición izquierda superior
       />
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
@@ -21,7 +21,7 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-base duration-500 text-zinc-400 hover:text-zinc-300"
             >
               {item.name}
             </Link>
@@ -39,7 +39,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block font-display animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-lg text-zinc-400 ">
           Creando{" "}
           <Link
             target="_blank"
