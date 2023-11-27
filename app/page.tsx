4f31f67632d7c2ea5733e27efb1ba63eac5033d2
FELIPE <contact@iapps.ai>
2023-11-27 03:49:58 -0500

    Solucion 2 sintaxis

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block font-display animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 mx-16 text-center animate-fade-in">
         <h2 className="text-lg text-zinc-400 font-poppins ">
-          Creando SOLUCIONES con Inteligencia Artificial para <br>EVOLUCIONAR tu empresa y prepararla hacia el FUTURO.</br>
+          Creando SOLUCIONES con Inteligencia Artificial para EVOLUCIONAR tu empresa y prepararla hacia el FUTURO.
         </h2>
       </div>
     </div>
