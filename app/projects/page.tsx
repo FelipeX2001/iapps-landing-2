7e50f41509bfe356157708471337b963af142457
FELIPE <contact@iapps.ai>
2023-11-28 00:00:44 -0500

    Redaccion de aiprop mdx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -42,11 +42,11 @@ export default async function ProjectsPage() {
       <Navigation />
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+          <h2 className="text-3xl font-display tracking-tight text-zinc-100 sm:text-4xl">
             PROYECTOS
           </h2>
-          <p className="mt-4 text-zinc-400 font-normal">
-            Finalizados y en Proceso
+          <p className="mt-4 text-zinc-400 font-poppins font-regular">
+            Construyendo el FUTURO con tu Inteligencia y la Artificial
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
@@ -64,7 +64,7 @@ export default async function ProjectsPage() {
                         }).format(new Date(featured.date))}
                       </time>
                     ) : (
-                      <span>SOON</span>
+                      <span>PRONTO</span>
                     )}
                   </div>
                   <span className="flex items-center gap-1 text-xs text-zinc-500">
@@ -77,11 +77,11 @@ export default async function ProjectsPage() {
 
                 <h2
                   id="featured-post"
-                  className="mt-4 text-3xl font-display text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                  className="mt-4 text-3xl text-zinc-100 group-hover:text-white sm:text-4xl font-display"
                 >
                   {featured.title}
                 </h2>
-                <p className="mt-4 leading-8 duration-150 text-zinc-400 font-display group-hover:text-zinc-300">
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 font-poppins font-regular group-hover:text-zinc-300">
                   {featured.description}
                 </p>
                 <div className="absolute bottom-4 md:bottom-8">
